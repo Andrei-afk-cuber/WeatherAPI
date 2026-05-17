b8bcf0d4d0953794b785debe074446ae582ec24b
Andrei Lyakh <Edgar.digl@mail.ru>
2026-05-17 13:56:24 +0300

    Fixed some bugs

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 
+from .models import UserRequest
 
-class WeatherSerializer(serializers.Serializer):
-    city = serializers.CharField()
-    main = serializers.CharField()
-    description = serializers.CharField()
-    temperature = serializers.IntegerField()
+class WeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRequest
+        fields = ['city', 'weather', 'weather_description', 'temperature']
+
+    def to_internal_value(self, data):
+        return {
+            'city': data.get('city'),
+            'weather': data.get('main'),
+            'temperature': data.get('temperature'),
+            'weather_description': data.get('description'),
+        }
--- weather/views.py
+++ weather/views.py
@@ -14,12 +14,15 @@ class WeatherView(APIView):
             try:
                 weather_data = get_weather(city)
                 weather_data['city'] = city
-
-                serializer = WeatherSerializer(data=weather_data)
-
-                if serializer.is_valid():
-                    return Response(serializer.data, status=status.HTTP_200_OK)
             except Exception as e:
-                return Response({'error': 'e'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': 'add correct city at query params'}, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = WeatherSerializer(data=weather_data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'error':'add correct city at query params'}, status=status.HTTP_400_BAD_REQUEST)
